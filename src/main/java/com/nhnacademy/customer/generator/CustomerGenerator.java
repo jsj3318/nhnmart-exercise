15143f8dad8604249937a95c41fbafff5605948e
조승주 <tmdwn3318@gmail.com>
2024-09-11 17:35:41 +0900

    step 4 clear

--- src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
+++ src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
@@ -37,11 +37,13 @@ public class CustomerGenerator implements Runnable {
 
     public CustomerGenerator(EnteringQueue enteringQueue) {
         //TODO#4-1 enteringQueue null 이면 'IllegalArgumentException' 발생하는지 검증 합니다.
-
+        if(enteringQueue == null){
+            throw new IllegalArgumentException();
+        }
 
         //TODO#4-2 enteringQueue, atomicId 를 0으로 초기화 합니다.
-        this.enteringQueue = null;
-        atomicId=null;
+        this.enteringQueue = enteringQueue;
+        atomicId = new AtomicLong(0);
 
     }
 
@@ -52,8 +54,17 @@ public class CustomerGenerator implements Runnable {
             - while 조건을 수정하세요.
             - 1초 간격으로 회원을 entringQueue의 대기열에 등록 합니다.
         */
-        while (true){
+        while (!Thread.currentThread().isInterrupted()){
             //1초 간격으로 회원을 entringQueue의 대기열에 등록 합니다.
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            Customer newCustomer = generate();
+            enteringQueue.addCustomer(newCustomer);
+            log.debug("Customer added:{}", newCustomer);
         }
     }
 
@@ -64,8 +75,12 @@ public class CustomerGenerator implements Runnable {
             - 회원이름은 random으로 생성 됩니다.
                - 회원이름 생성시 https://github.com/Devskiller/jfairy 이용해서 구현 합니다.
          */
+        Long id = atomicId.incrementAndGet();
+        Fairy fairy = Fairy.create();
+        Person person = fairy.person();
+        String name = person.getFullName();
 
-        Customer customer = null;
+        Customer customer = new Customer(id, name, DEFAULT_MONEY);
         return customer;
     }
 }
--- src/test/java/com/nhnacademy/customer/generator/CustomerGeneratorTest.java
+++ src/test/java/com/nhnacademy/customer/generator/CustomerGeneratorTest.java
@@ -29,17 +29,20 @@ class CustomerGeneratorTest {
     @BeforeEach
     void setUp() {
         //TODO#4-5 enteringQueue 대기열을 capacity = 5로 초기화 합니다.
-        enteringQueue = null;
+        enteringQueue = new EnteringQueue(5);
 
         //TODO#4-6 enteringQueue를 이용 해서 customerGeneratorr 객체를 생성 합니다.
-        customerGenerator = null;
+        customerGenerator = new CustomerGenerator(enteringQueue);
     }
 
     @Test
     @DisplayName("enteringQueue is null")
     void constructorTest(){
         //TODO#4-7 enteringQueue == null 이면 IllegalArgumentException 발생 하는지 검증 합니다.
-
+        EnteringQueue queue = null;
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+           CustomerGenerator generator = new CustomerGenerator(queue);
+        });
     }
 
     @Test
@@ -47,13 +50,14 @@ class CustomerGeneratorTest {
     void generatorTest() throws InterruptedException {
 
         //TODO#4-8 customerGenerator를 이용해서 customerGeneratorThread 초기화 하고, 실행 합니다.
-        Thread customerGeneratorThread = null;
+        Thread customerGeneratorThread = new Thread(customerGenerator);
+        customerGeneratorThread.start();
 
         //TODO#4-9 10초 대기 합니다.
-
+        Thread.sleep(10000);
 
         //TODO#4-10 customerGeneratorThread를 종료 합니다.
-
+        customerGeneratorThread.interrupt();
 
         while(customerGeneratorThread.isAlive()){
             Thread.yield();
@@ -65,9 +69,11 @@ class CustomerGeneratorTest {
         Assertions.assertAll(
             ()->{
                 //TODO#4-11 interrupt발생시 customerGeneratorThread 의 상태가  TERMINATED 상태인지 검증
+                Assertions.assertEquals(customerGeneratorThread.getState(), Thread.State.TERMINATED);
             },
             ()->{
                 //TODO#4-12 enteringQueue(대기열) 최대 Queue Size가 5 <-- 10초 동안 최대 5명의 고객이 대기열에 등록되었는지 검증 합니다.
+                Assertions.assertEquals(5, enteringQueue.getQueueSize());
             }
         );
     }
